e1d8e8bf255ada64fcf4e7841dae099df504c199
olliee2 <218727432+olliee2@users.noreply.github.com>
2025-07-09 23:10:36 +0100

    feat: replace span with bold element for time separator and remove redundant CSS styling

--- src/Selector.ts
+++ src/Selector.ts
@@ -71,11 +71,11 @@ export default class Selector {
         const durationInMinutes = Math.floor(song.duration / 60);
         if (durationInMinutes !== previousDurationInMinutes) {
           previousDurationInMinutes = durationInMinutes;
-          const span = document.createElement('span');
-          span.className = 'time-separator';
+          const b = document.createElement('b');
+          b.className = 'time-separator';
           const li = document.createElement('li');
-          span.textContent = durationInMinutes.toString();
-          li.append(span);
+          b.textContent = durationInMinutes.toString();
+          li.append(b);
           frag.append(li);
         }
       }
